06d8871398e9767b29042bc0dac021418f338930
DoomAngelBlade <bladeanser@gmail.com>
2023-07-27 20:07:49 +0300

    fix

--- dremkas/settings.py
+++ dremkas/settings.py
@@ -3,7 +3,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from mainapp.diadoc_api import DiadocApi
 from mainapp.dremkas_api import DreamKasApi
-
+import dotenv
+dotenv.load_dotenv(override=True)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
