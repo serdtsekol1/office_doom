9a79a828933ca070e109aa0cc36eaeedd388f8d5
DoomAngelBlade <bladeanser@gmail.com>
2024-03-18 01:18:44 +0300

    Шаблоны.

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -5,7 +5,8 @@ from mainapp.views import get_index_page, test, invoices, manual_invoice, good_g
     generate_goods_report, edit_existing_report, invoice_reports, generate_invoice_report, update_all_goods, generate_xlsx_file_for_printer, display_all_goods_for_printer, products, \
     update_one_product, create_or_change_printer_code_for_product, change_printer_file_location, delete_all_suppliers, update_all_suppliers, set_store_id, update_stores_and_devices, \
     find_invoice_duplicates, delete_broken_suppliers, show_duplicate_diadoc_invoices, create_or_change_short_name_for_product, gmail_presets, update_gmail_preset, create_gmail_preset, stores, \
-    update_store, update_diadoc_invoices_v2, invoices_diadoc_v2, diadoc_presets, create_diadoc_preset, update_diadoc_preset, create_document_from_diadoc_v2, create_documents_from_gmail_message_v2
+    update_store, update_diadoc_invoices_v2, invoices_diadoc_v2, diadoc_presets, create_diadoc_preset, update_diadoc_preset, create_document_from_diadoc_v2, create_documents_from_gmail_message_v2, \
+    delete_all_stores
 
 urlpatterns = [
     path('', get_index_page, name="index"),
@@ -17,6 +18,7 @@ urlpatterns = [
     path('update_stores_and_devices/', update_stores_and_devices, name="update_stores_and_devices"),
     path('stores/', stores, name="stores"),
     path('update_store/', update_store, name="update_store"),
+    path('delete_all_stores/', delete_all_stores, name="delete_all_stores"),
 
 
     path('manual_invoice/', manual_invoice, name="manual_invoice"),
